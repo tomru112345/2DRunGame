8017626c4a4ad692285fa8e4340e21b8682fca6a
tomru112345 <kazt23@gmail.com>
2021-12-24 18:44:43 +0900

    add anim

--- Assets/Scripts/PlaneScript.cs
+++ Assets/Scripts/PlaneScript.cs
@@ -14,8 +14,10 @@ public class PlaneScript : MonoBehaviour
 
     // float default_y = -5;
 
-    void Start() {
-        for (int i = 0; i < step.Length; i++) {
+    void Start()
+    {
+        for (int i = 0; i < step.Length; i++)
+        {
             step[i] = Instantiate(Plane, new Vector3(4 * i, 0, 0), Quaternion.identity);
             step[i].tag = "Ground";
             step[i].transform.parent = GameObject.Find("GroundList").transform;
@@ -23,24 +25,31 @@ public class PlaneScript : MonoBehaviour
         // Instantiate(ThirdPersonCharactor, new Vector3(8, 5, 0), Quaternion.identity);
     }
 
-    void Update() {
-        for (int i = 0; i < step.Length; i++) {
+    void Update()
+    {
+        for (int i = 0; i < step.Length; i++)
+        {
             step[i].gameObject.transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
-            if (step[i].gameObject.transform.position.x < disappear) {
+            if (step[i].gameObject.transform.position.x < disappear)
+            {
                 ChangeScale(i);
                 step[i].gameObject.transform.position = new Vector3(respawn, 0, 0);
             }
         }
     }
-    void ChangeScale(int i) {
+    void ChangeScale(int i)
+    {
         int x = (i + 9) % 10; //(i + 9) を 10 で割った余り
-        if (step[x].transform.localScale.y == 0.5) {
+        if (step[x].transform.localScale.y == 0.5)
+        {
             step[i].transform.localScale = step[x].transform.localScale + new Vector3(0, Random.Range(0, 2), 0);
         }
-        else if (step[x].transform.localScale.y >= 10) {
+        else if (step[x].transform.localScale.y >= 10)
+        {
             step[i].transform.localScale = step[x].transform.localScale + new Vector3(0, Random.Range(-1, 1), 0);
         }
-        else {
+        else
+        {
             step[i].transform.localScale = step[x].transform.localScale + new Vector3(0, Random.Range(-1, 2), 0);
         }
     }
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -13,50 +13,59 @@ public class PlayerScript : MonoBehaviour
     //ゲームオーバー判定
     private bool isGameOver = false;
     [SerializeField] private int minute;
-	[SerializeField] private float seconds;
-	//　前のUpdateの時の秒数
-	private float oldSeconds;
-	//　タイマー表示用テキスト
-	[SerializeField] public GameObject timerText;
-    [SerializeField] public GameObject GameOverText;
-    [SerializeField] public GameObject QuitButton;
+    [SerializeField] private float seconds;
+    //　前のUpdateの時の秒数
+    private float oldSeconds;
+    //　タイマー表示用テキスト
+    [SerializeField] private GameObject timerText;
+    [SerializeField] private GameObject GameOverText;
+    [SerializeField] private GameObject QuitButton;
 
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody>();
 
         minute = 0;
-		seconds = 0f;
-		oldSeconds = 0f;
-		timerText.GetComponent<Text>().text = minute.ToString("00") + ":" + ((int) seconds).ToString ("00");
+        seconds = 0f;
+        oldSeconds = 0f;
+        timerText.GetComponent<Text>().text = minute.ToString("00") + ":" + ((int)seconds).ToString("00");
         GameOverText.GetComponent<Text>().text = "";
+        animator = gameObject.GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         seconds += Time.deltaTime;
-		if(seconds >= 60f) {
-			minute++;
-			seconds = seconds - 60;
-		}
-		//　値が変わった時だけテキストUIを更新
-		if((int)seconds != (int)oldSeconds && !isGameOver) {
-			timerText.GetComponent<Text>().text = minute.ToString("00") + ":" + ((int) seconds).ToString ("00");
-		}
-		oldSeconds = seconds;
-
-        if (JumpCount <= 1)//着地しているとき
+        if (seconds >= 60f)
         {
-            if(Input.GetKeyDown("space"))
+            minute++;
+            seconds = seconds - 60;
+        }
+        //　値が変わった時だけテキストUIを更新
+        if ((int)seconds != (int)oldSeconds && !isGameOver)
+        {
+            timerText.GetComponent<Text>().text = minute.ToString("00") + ":" + ((int)seconds).ToString("00");
+        }
+        oldSeconds = seconds;
+
+        if (JumpCount < 2)//着地しているとき
+        {
+            if (Input.GetKeyDown("space") || Input.GetMouseButtonDown(0))
             {
+                animator.ResetTrigger("Jump");
+                animator.enabled = false;
                 JumpCount++;//  isGroundをfalseにする
                 myRigidbody.AddForce(Vector3.up * upForce); //上に向かって力を加える
+                animator.enabled = true;
+                animator.SetTrigger("Jump");
             }
         }
 
-        if (myRigidbody.transform.position.y < -1){
+        if (myRigidbody.transform.position.y < -1)
+        {
             //ゲームオーバー
             isGameOver = true;
             GameOverText.GetComponent<Text>().text = timerText.GetComponent<Text>().text;
@@ -64,12 +73,6 @@ public class PlayerScript : MonoBehaviour
             timerText.SetActive(false);
             QuitButton.SetActive(true);
         }
-
-        //ゲームオーバー状態で画面がクリックされたとき
-        // if(isGameOver && Input.GetKeyDown("space"))
-        // {
-        //     Restart();
-        // }
     }
 
     void OnCollisionEnter(Collision other) //地面に触れた時の処理
@@ -79,11 +82,4 @@ public class PlayerScript : MonoBehaviour
             JumpCount = 0; //isGroundをtrueにする
         }
     }
-
-    //シーンを再読み込みする
-    // private void Restart()
-    // {
-    //     // Sceneの読み直し
-    //     SceneManager.LoadScene("TitleScene");
-    // }
 }
